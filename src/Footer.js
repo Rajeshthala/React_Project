5df3fb5d09b74c7a22c9d19cf250ffc03363ea28
RAJESH <124981833+Rajeshthala@users.noreply.github.com>
2023-11-21 09:20:47 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'rgba(0, 123, 255, 0.8)', padding: '20px', textAlign: 'center', color: '#fff' }}>
+      <p>&copy; 2023 Your App Name. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
